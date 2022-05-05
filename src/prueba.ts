6245a3c706151aabe901c10e6e1361076caf58ad
Alejandro Perez <alu0101215310@ull.edu.es>
2022-05-05 08:09:29 +0000

    init

---
+++ src/prueba.ts
@@ -0,0 +1,12 @@
+/**
+ * Adds to numbers
+ * @param firstNumber First operand
+ * @param secondNumber Second operand
+ * @returns The addition
+ */
+export function add(firstNumber: number, secondNumber: number) {
+  return firstNumber + secondNumber;
+}
+
+console.log(`add(1,7): ${add(1, 7)}`);
+console.log(`add(1.7,3.5): ${add(1.7, 3.5)}`);
